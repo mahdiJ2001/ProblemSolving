81d36d9c9f8430b78b1b581fd8e59609dacd4c0f
mahdiJ2001 <130222289+mahdiJ2001@users.noreply.github.com>
2024-03-22 00:27:32 +0100

    Add files via upload

---
+++ A. Shuffle Party.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+
+int main(){
+	int t ; 
+	cin>>t;
+	while(t--){
+		long long n ; 
+		cin>>n;
+		long long x=log2(n);
+		long long res;
+		res=int(x);
+		
+		cout<<fixed << setprecision(0)<<pow(2,res)<<endl;
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	return 0 ; 
+}
